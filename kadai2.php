a0800fd5128b0303d00c480084bc44362983eae8
名前 <>
2020-07-09 02:53:19 +0000

    kadai3

---
+++ kadai2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF8">
+        <title>kadai2</title>
+        
+    </head>
+    <body>
+        <?php 
+        $name="helio";
+        if($name=="helio"){
+            echo "私は、{$name}です。";
+        }else{
+            echo "あなたの名前ではありません。";
+        }
+        echo "\n";
+        $count=1;
+        for($i=1;$i<10000;$i++){
+            echo $count+=$i;
+            $count++;
+        }
+        echo "\n";
+        $count2=1;
+        $fruits=['apple','banana','melon','orange','pear'];
+        foreach ($fruits as $fruit){
+            echo "{$count2}=>{$fruit}";
+            echo "\n";
+            $count+=1;
+        }
+        echo "\n";
+        $start=1;
+        $end=100;
+        for($i=$start;$i<$end;$i++){
+            if($i % 5==0){
+                echo $i;
+                echo "\n";
+            }
+        }
+        
+        
+        
+        ?>
+    </body>
+</html>
