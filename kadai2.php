57f215675111266f4d20641505cca2c4232fa539
名前 <>
2020-07-11 02:31:18 +0000

    kadai3

--- kadai2.php
+++ kadai2.php
@@ -30,7 +30,7 @@
         echo "\n";
         $start=1;
         $end=100;
-        for($i=$start;$i<$end;$i++){
+        for($i=$start;$i<=$end;$i++){
             if($i % 5==0){
                 echo $i;
                 echo "\n";
